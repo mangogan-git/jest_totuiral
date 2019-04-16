1b985fe847b82b20d49453ca11243db70bce27dd
mango <mango@xsgames.com>
2019-04-16 18:01:06 +0800

    add babel, fail test case

--- getting_start/test/add.test.js
+++ getting_start/test/add.test.js
@@ -1,15 +1,15 @@
-import { add } from '../test/add.test';
+import { add } from '../src/add';
 
 describe('testing my function', () => {
     test('2+3', () => {
-        expect(add(2 + 3)).toBe(5);
+        expect(add(2, 3)).toBe(5);
     });
 
     it('3+3', () => {
-        expect(add(3 + 3)).toBe(6);
-    })
+        expect(add(3,3)).toBe(6);
+    });
 
     test('test case fail', () => {
-        expect(add(1 + 1)).toBe(7);
-    })
-})
+        expect(add(1,  1)).toBe(7);
+    });
+});
