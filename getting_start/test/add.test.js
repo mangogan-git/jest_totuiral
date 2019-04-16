a29cd0aaa491a88e7cfb00254f45b8e1e9d2edbd
mango <mango@xsgames.com>
2019-04-16 17:03:57 +0800

    init getting_start

---
+++ getting_start/test/add.test.js
@@ -0,0 +1,15 @@
+import { add } from '../test/add.test';
+
+describe('testing my function', () => {
+    test('2+3', () => {
+        expect(add(2 + 3)).toBe(5);
+    });
+
+    it('3+3', () => {
+        expect(add(3 + 3)).toBe(6);
+    })
+
+    test('test case fail', () => {
+        expect(add(1 + 1)).toBe(7);
+    })
+})
